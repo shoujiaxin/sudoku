0f96ba47dcd1a5a1699656d611ec235d18507017
Jiaxin Shou <shoujiaxin@gmail.com>
2023-12-28 23:10:58 +0800

    feat: add initial project

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Jiaxin Shou on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
