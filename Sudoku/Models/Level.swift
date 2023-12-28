ff0e06d6209b68323476919703038d661e51f4fe
Jiaxin Shou <shoujiaxin@gmail.com>
2023-12-29 00:35:53 +0800

    1

---
+++ Sudoku/Models/Level.swift
@@ -0,0 +1,20 @@
+//
+//  Level.swift
+//  Sudoku
+//
+//  Created by Jiaxin Shou on 2023/12/29.
+//
+
+import Foundation
+
+enum Level: String, CaseIterable {
+    case easy
+
+    case medium
+
+    case hard
+
+    case expert
+
+    case master
+}
